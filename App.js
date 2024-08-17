b1ab03b1ab53a2fed046c453ec0720aeb259c2b2
Saikiran Dasari <dasarisaikiran786@gmail.com>
2024-08-17 11:24:31 +0530

    Rendering the Parent in the root

--- App.js
+++ App.js
@@ -4,18 +4,16 @@ const heading = React.createElement(
   "Hello world from React"
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-
-const parent=React.createElement("div",{id:"parent"},
-  React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},"I am an h1 tag"),
-    React.createElement("h2",{},"I am h2 tag")
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
   ]),
-  React.createElement("div",{id:"child2"},[
-    React.createElement("h1",{},"I am on h1 tag"),
-    React.createElement("h2",{},"I am h2 tag"),
-  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am on h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ])
 );
-
-
+root.render(parent);
